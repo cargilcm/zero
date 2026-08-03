3ece07f713e047f3af1bb4e861e9addf0b94c926
Chris Cargile <follybeachris@gmail.com>
2026-08-03 08:39:41 -0400

    add compose plugin to root build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -34,6 +34,7 @@ plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.android.library) apply false
     alias(libs.plugins.publish) apply false
+	id("org.jetbrains.kotlin.plugin.compose") version "2.3.10"
 }
 
 buildscript {
